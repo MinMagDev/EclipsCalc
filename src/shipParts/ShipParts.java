ec150978f226b4cd6a19f64e11d5e7c0ee08e9b2
minmagdev <levi.drieling@gmail.com>
2025-09-17 16:39:36 +0200

    Added Base Ships as Getters

--- src/shipParts/ShipParts.java
+++ src/shipParts/ShipParts.java
@@ -5,52 +5,52 @@ public class ShipParts {
     //List of all ShipParts
 
     //Weapons
-    public final WeaponPart ION_CANNON        = new WeaponPart(1);
-    public final WeaponPart PLASMA_CANNON     = new WeaponPart(2,2);
-    public final WeaponPart SOLITON_CANNON    = new WeaponPart(3,3);
-    public final WeaponPart ANTIMATTER_CANNON = new WeaponPart(4,4);
+    public static final WeaponPart ION_CANNON        = new WeaponPart(1);
+    public static final WeaponPart PLASMA_CANNON     = new WeaponPart(2,2);
+    public static final WeaponPart SOLITON_CANNON    = new WeaponPart(3,3);
+    public static final WeaponPart ANTIMATTER_CANNON = new WeaponPart(4,4);
 
-    public final WeaponPart FLUX_MISSILE   = new WeaponPart(true, 1,2,0,1);
-    public final WeaponPart PLASMA_MISSILE = new WeaponPart(true, 2,2,1,0);
+    public static final WeaponPart FLUX_MISSILE   = new WeaponPart(true, 1,2,0,1);
+    public static final WeaponPart PLASMA_MISSILE = new WeaponPart(true, 2,2,1,0);
 
-    public final WeaponPart ION_MISSILE        = new WeaponPart(true, 1, 3,0,0);
-    public final WeaponPart SOLITON_MISSILE    = new WeaponPart(true, 3, 1,0,0);
-    public final WeaponPart ANTIMATTER_MISSILE = new WeaponPart(true, 4, 1,0,0);
-    public final WeaponPart ION_TURRET         = new WeaponPart(false, 2,2,0,0);
-    public final WeaponPart SOLITON_CHARGER    = new WeaponPart(false, 3,1,1,0);
+    public static final WeaponPart ION_MISSILE        = new WeaponPart(true, 1, 3,0,0);
+    public static final WeaponPart SOLITON_MISSILE    = new WeaponPart(true, 3, 1,0,0);
+    public static final WeaponPart ANTIMATTER_MISSILE = new WeaponPart(true, 4, 1,0,0);
+    public static final WeaponPart ION_TURRET         = new WeaponPart(false, 2,2,0,0);
+    public static final WeaponPart SOLITON_CHARGER    = new WeaponPart(false, 3,1,1,0);
     //TODO: Missing Ancient Weapon Ship Parts
 
     //Computers
-    public final ComputerPart ELECTRON_COMPUTER = new ComputerPart(1,0);
-    public final ComputerPart POSITRON_COMPUTER = new ComputerPart(2,1);
-    public final ComputerPart GLUON_COMPUTER    = new ComputerPart(3,2);
+    public static final ComputerPart ELECTRON_COMPUTER = new ComputerPart(1,0);
+    public static final ComputerPart POSITRON_COMPUTER = new ComputerPart(2,1);
+    public static final ComputerPart GLUON_COMPUTER    = new ComputerPart(3,2);
     //TODO: Missing ancient computer ship Parts
 
     //Shields
-    public final ShieldPart GAUSS_SHIELD      = new ShieldPart(1,0);
-    public final ShieldPart PHASE_SHIELD      = new ShieldPart(2,1);
-    public final ShieldPart ABSORPTION_SHIELD = new ShieldPart(1,-4);
-    public final ShieldPart INVERSION_SHIELD  = new ShieldPart(2,2);
+    public static final ShieldPart GAUSS_SHIELD      = new ShieldPart(1,0);
+    public static final ShieldPart PHASE_SHIELD      = new ShieldPart(2,1);
+    public static final ShieldPart ABSORPTION_SHIELD = new ShieldPart(1,-4);
+    public static final ShieldPart INVERSION_SHIELD  = new ShieldPart(2,2);
 
-    public final ShieldPart FLUX_SHIELD       = new ShieldPart(3,2,1);
+    public static final ShieldPart FLUX_SHIELD       = new ShieldPart(3,2,1);
 
     //Hulls
-    public final HullPart HULL           = new HullPart(1,0);
-    public final HullPart IMPROVED_HULL  = new HullPart(2,0);
-    public final  HullPart CONIFOLD_HULL = new HullPart(3,3);
+    public static final HullPart HULL           = new HullPart(1,0);
+    public static final HullPart IMPROVED_HULL  = new HullPart(2,0);
+    public static final  HullPart CONIFOLD_HULL = new HullPart(3,3);
 
     //Drives
-    public final DrivePart NUCLEAR_DRIVE    = new DrivePart(1,1,1);
-    public final DrivePart FUSION_DRIVE     = new DrivePart(2,2,2);
-    public final DrivePart TACHYON_DRIVE    = new DrivePart(3,3,3);
-    public final DrivePart TRANSITION_DRIVE = new DrivePart(3,0,0);
+    public static final DrivePart NUCLEAR_DRIVE    = new DrivePart(1,1,1);
+    public static final DrivePart FUSION_DRIVE     = new DrivePart(2,2,2);
+    public static final DrivePart TACHYON_DRIVE    = new DrivePart(3,3,3);
+    public static final DrivePart TRANSITION_DRIVE = new DrivePart(3,0,0);
 
     //Energy
-    public final EnergyPart NUCLEAR_SOURCE    = new EnergyPart(3);
-    public final EnergyPart TACHYON_SOURCE    = new EnergyPart(9);
-    public final EnergyPart FUSION_SOURCE     = new EnergyPart(6);
-    public final EnergyPart HYPERGRID_SOURCE  = new EnergyPart(11);
-    public final EnergyPart ZERO_POINT_SOURCE = new EnergyPart(12);
+    public static final EnergyPart NUCLEAR_SOURCE    = new EnergyPart(3);
+    public static final EnergyPart TACHYON_SOURCE    = new EnergyPart(9);
+    public static final EnergyPart FUSION_SOURCE     = new EnergyPart(6);
+    public static final EnergyPart HYPERGRID_SOURCE  = new EnergyPart(11);
+    public static final EnergyPart ZERO_POINT_SOURCE = new EnergyPart(12);
 
 
 
--- src/ships/Ship.java
+++ src/ships/Ship.java
@@ -1,11 +1,29 @@
 package ships;
 
 import shipParts.ShipPart;
+import shipParts.ShipParts;
 
 public class Ship {
+
+    //List of all Ships
+    private static final int INTERCEPTOR_SIZE = 4;
+    private static final int CRUISER_SIZE     = 6;
+    private static final int DREADNOUGHT_SIZE = 8;
+
+    private static final int INTERCEPTOR_INI = 2;
+    private static final int CRUISER_INI     = 1;
+    private static final int DREADNOUGHT_INI = 0;
+
+
     private int partAmount;
     private int baseInititive;
-    private ShipPart[] shipParts = new ShipPart[partAmount];
+    private ShipPart[] shipParts;
+
+    public Ship(int size, int baseInititive){
+        partAmount = size;
+        this.baseInititive = baseInititive;
+        shipParts = new ShipPart[partAmount];
+    }
 
     public void replacePart(int index, ShipPart part) {
         shipParts[index] = part;
@@ -17,5 +35,34 @@ public class Ship {
             res += part.getInititiveBonus();
         }
         return getInititive();
+    }
+
+
+    //Getter for Base Ships
+    public static void getBaseInterceptor(){
+        Ship ship = new Ship(INTERCEPTOR_SIZE, INTERCEPTOR_INI);
+        ship.replacePart(0, ShipParts.NUCLEAR_SOURCE);
+        ship.replacePart(1, ShipParts.ION_CANNON);
+        ship.replacePart(2, ShipParts.NUCLEAR_DRIVE);
+    }
+
+    public static void getBaseCruiser(){
+        Ship ship = new Ship(CRUISER_SIZE, CRUISER_INI);
+        ship.replacePart(0, ShipParts.ELECTRON_COMPUTER);
+        ship.replacePart(1, ShipParts.NUCLEAR_SOURCE);
+        ship.replacePart(2, ShipParts.ION_CANNON);
+        ship.replacePart(3, ShipParts.HULL);
+        ship.replacePart(5, ShipParts.NUCLEAR_DRIVE);
+    }
+
+    public static void getBaseDreadnought(){
+        Ship ship = new Ship(DREADNOUGHT_SIZE,DREADNOUGHT_INI);
+        ship.replacePart(0, ShipParts.ELECTRON_COMPUTER);
+        ship.replacePart(1, ShipParts.NUCLEAR_SOURCE);
+        ship.replacePart(2, ShipParts.ION_CANNON);
+        ship.replacePart(3, ShipParts.HULL);
+        ship.replacePart(4, ShipParts.ION_CANNON);
+        ship.replacePart(5, ShipParts.HULL);
+        ship.replacePart(7, ShipParts.NUCLEAR_DRIVE);
     }
 }
